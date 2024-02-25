376840d4adfef1b91b4254b0161c4318b16d888e
achalpathak <pathak.achal5@gmail.com>
2024-02-26 00:29:35 +0530

    added vim hardmode

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -19,3 +19,13 @@ vim.keymap.set("n", "<leader>cj", ":diffget 1<CR>") -- get diff from left (local
 vim.keymap.set("n", "<leader>ck", ":diffget 3<CR>") -- get diff from right (remote) during merge
 vim.keymap.set("n", "<leader>cn", "]c") -- next diff hunk
 vim.keymap.set("n", "<leader>cp", "[c") -- previous diff hunk
+
+--
+-- inoremap <C-h> <Left>
+-- inoremap <C-j> <Down>
+-- inoremap <C-k> <Up>
+-- inoremap <C-l> <Right>
+-- cnoremap <C-h> <Left>
+-- cnoremap <C-j> <Down>
+-- cnoremap <C-k> <Up>
+-- cnoremap <C-l> <Right>
