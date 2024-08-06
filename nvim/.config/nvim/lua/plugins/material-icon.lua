f79d8e9791ff27c8f272655836e1c8219f5aa429
achal.pathak <achal.pathak@simplisafe.com>
2024-08-07 04:18:48 +0530

    fixes

--- nvim/.config/nvim/lua/plugins/material-icon.lua
+++ nvim/.config/nvim/lua/plugins/material-icon.lua
@@ -1,21 +1,21 @@
 return {
   "DaikyXendo/nvim-material-icon",
   config= function()
-    require("nvim-material-icon").setup({
-        override = {
-          zsh = {
-          icon = "",
-          color = "#428850",
-          cterm_color = "65",
-          name = "Zsh",
-          },
-        },
-      -- globally enable different highlight colors per icon (default to true)
-      -- if set to false all icons will have the default icon's color
-      color_icons = true,
-      -- globally enable default icons (default to false)
-      -- will get overriden by `get_icons` option
-      default = true,
-})
+--     require("nvim-material-icon").setup({
+--         override = {
+--           zsh = {
+--           icon = "",
+--           color = "#428850",
+--           cterm_color = "65",
+--           name = "Zsh",
+--           },
+--         },
+--       -- globally enable different highlight colors per icon (default to true)
+--       -- if set to false all icons will have the default icon's color
+--       color_icons = true,
+--       -- globally enable default icons (default to false)
+--       -- will get overriden by `get_icons` option
+--       default = true,
+-- })
   end,
 }
