9cfec844881b4cc22401bd2127acb284a104957d
Achal Pathak <pathak.achal5@gmail.com>
2025-07-03 23:30:20 +0530

    added code folding and lazygit

--- nvim/.config/nvim/lua/achal/plugins/extras.lua
+++ nvim/.config/nvim/lua/achal/plugins/extras.lua
@@ -98,5 +98,30 @@ return {
 		config = function()
 			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Show Undo Tree" })
 		end,
+		-- Comments
+		{
+			"echasnovski/mini.comment",
+			version = false,
+			dependencies = {
+				"JoosepAlviste/nvim-ts-context-commentstring",
+			},
+			config = function()
+				-- disable the autocommand from ts-context-commentstring
+				require("ts_context_commentstring").setup({
+					enable_autocmd = false,
+				})
+
+				require("mini.comment").setup({
+					-- tsx, jsx, html , svelte comment support
+					options = {
+						custom_commentstring = function()
+							return require("ts_context_commentstring.internal").calculate_commentstring({
+								key = "commentstring",
+							}) or vim.bo.commentstring
+						end,
+					},
+				})
+			end,
+		},
 	},
 }
--- nvim/.config/nvim/lua/achal/plugins/git.lua
+++ nvim/.config/nvim/lua/achal/plugins/git.lua
@@ -17,5 +17,25 @@ return {
 			virtual_text_column = 2,
 			message_when_not_committed = "",
 		},
+		-- Lazy git
+		{
+			"kdheepak/lazygit.nvim",
+			enabled = true,
+			cmd = {
+				"LazyGit",
+				"LazyGitConfig",
+				"LazyGitCurrentFile",
+				"LazyGitFilter",
+				"LazyGitFilterCurrentFile",
+			},
+			-- window border thing
+			dependencies = {
+				"nvim-lua/plenary.nvim",
+			},
+			-- setting up with keys={} allows plugin to load when command runs at the start
+			keys = {
+				{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+			},
+		},
 	},
 }
